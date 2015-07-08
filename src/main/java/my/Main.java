89bdb361f5632fc56e0d58009d8260ad1678c43e
Hu Dongqing <dhu>
2015-07-08 15:09:42 +0800

    Comments

--- src/main/java/my/Main.java
+++ src/main/java/my/Main.java
@@ -15,6 +15,7 @@ import com.alibaba.druid.pool.DruidDataSourceFactory;
 import static java.lang.System.currentTimeMillis;
 import static java.lang.System.out;
 
+// Mainly simulating the scene of a heavy-load RPC system
 public class Main {
   static DataSource dataSource;
   static AtomicInteger count = new AtomicInteger(0);
@@ -27,16 +28,16 @@ public class Main {
   }
 
   static void multiThreads() throws InterruptedException {
-    int threads = 200;
-    ExecutorService es = Executors.newFixedThreadPool(threads);
-    for (int i = 0; i < threads; i++) {
+    int THREADS = 200;
+    ExecutorService es = Executors.newFixedThreadPool(THREADS);
+    for (int i = 0; i < THREADS; i++) {
       es.submit(() -> {
         for(int j = 0; j < 20; j++) {
           try {
             hold(10);
             hold(10);
             hold(10);
-            Thread.sleep(200);
+            Thread.sleep(200); // simulates idle thread after completing a task
           } catch (Exception e) {
             e.printStackTrace();
           }
@@ -48,6 +49,7 @@ public class Main {
     System.out.println("Long wait: " + count.get());
   }
 
+  // SQL execution holds the connection
   static void hold(long time) throws SQLException, InterruptedException {
     long start = currentTimeMillis();
     Connection conn = dataSource.getConnection();
@@ -56,7 +58,7 @@ public class Main {
       out.print("\n"+cost+" ms ");
       count.incrementAndGet();
     }
-    Thread.sleep(time);
+    Thread.sleep(time); // simulates SQL execution
     conn.close();
   }
 }
